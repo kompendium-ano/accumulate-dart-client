7dc764067294db680440854e42fdc6142cb874a5
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2021-02-18 13:10:00 +0300

    update token info

--- lib/src/v2/responses/resp_token_get.dart
+++ lib/src/v2/responses/resp_token_get.dart
@@ -1,6 +1,8 @@
 
 import 'dart:convert';
 
+import 'package:accumulate_api/src/v2/responses/data.dart';
+
 class TokenAccountGetResponse {
   TokenAccountGetResponse({
     this.data,
@@ -28,107 +30,5 @@ class TokenAccountGetResponse {
   //     : data = json['data'],
   //       type = json['type'];
 
-
-}
-
-
-// String url = res.result["data"]["url"];
-// String tokenUrl = res.result["data"]["tokenUrl"];
-// String balance = res.result["data"]["balance"];
-// int txcount = res.result["data"]["txCount"];
-// int nonce = res.result["data"]["nonce"];
-// int creditBalance = res.result["data"]["creditBalance"];
-
-class Data {
-  Data({
-    this.url,
-    this.tokenUrl,
-    this.balance,
-    this.txcount,
-    this.nonce,
-    this.creditBalance
-  });
-
-  int? balance;
-  int? txcount;
-  int? nonce;
-  int? creditBalance;
-  String? tokenUrl;
-  String? url;
-
-  factory Data.fromRawJson(String str) => Data.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
-  factory Data.fromJson(Map<String, dynamic> json) => Data(
-    balance: json["balance"],
-    tokenUrl: json["tokenURL"],
-    url: json["url"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "balance": balance,
-    "tokenURL": tokenUrl,
-    "url": url,
-  };
-}
-
-class DataDirectory {
-  DataDirectory({
-    this.url,
-    this.tokenUrl,
-    this.balance,
-    this.keybooksCount,
-    this.tokenAccountsCount,
-    this.creditBalance,
-    this.entities
-  });
-
-  int? balance;
-  int? keybooksCount;
-  int? tokenAccountsCount;
-  int? creditBalance;
-  String? tokenUrl;
-  String? url;
-  List<String>? entities;
-
-  factory DataDirectory.fromRawJson(String str) => DataDirectory.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
-  factory DataDirectory.fromJson(Map<String, dynamic> json) => DataDirectory(
-    balance: json["balance"],
-    tokenUrl: json["tokenURL"],
-    url: json["url"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "balance": balance,
-    "tokenURL": tokenUrl,
-    "url": url,
-  };
-}
-
-class DataKeybook {
-  DataKeybook({
-    this.url,
-    this.mdroot,
-  });
-
-  String? url;
-  String? mdroot;
-
-}
-
-class DataKeyPage {
-  DataKeyPage({
-    this.url,
-    this.mdroot,
-    this.balanceCredits,
-  });
-
-  int? balanceCredits;
-  String? url;
-  String? mdroot;
 
 }
