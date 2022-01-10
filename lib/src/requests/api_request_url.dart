bd833406cb504e2c27f7aa2d338586c158759d72
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2022-01-10 18:16:01 +0200

    init requests; add url structure

---
+++ lib/src/requests/api_request_url.dart
@@ -0,0 +1,14 @@
+
+class ApiRequestUrl {
+  final String url;
+
+  ApiRequestUrl(this.url);
+
+  ApiRequestUrl.fromJson(Map<String, dynamic> json)
+      : url = json['url'];
+
+  Map<String, dynamic> toJson() => {
+    'url': url,
+  };
+
+}
