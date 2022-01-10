7734377713bfe507b51c263ab51eddc0c5100669
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2022-01-10 22:39:30 +0200

    (fear) add pagination request

--- lib/src/requests/api_request_url.dart
+++ lib/src/requests/api_request_url.dart
@@ -1,14 +1,17 @@
 
 class ApiRequestUrl {
   final String url;
+  final bool wait;
 
-  ApiRequestUrl(this.url);
+  ApiRequestUrl(this.url, this.wait);
 
   ApiRequestUrl.fromJson(Map<String, dynamic> json)
-      : url = json['url'];
+      : url  = json['url'],
+        wait = json['wait'];
 
   Map<String, dynamic> toJson() => {
     'url': url,
+    'wait': wait
   };
 
 }
