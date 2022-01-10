ebab47f4a2b4f39df7931e1c38a66ea7ed413105
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2022-01-10 22:40:20 +0200

    (feat) init adi structure

---
+++ lib/src/requests/adi/api_request_adi.dart
@@ -0,0 +1,12 @@
+class ApiRequestADI {
+  final String url;
+  final String publicKeyHash;
+
+  ApiRequestADI(this.url, this.publicKeyHash);
+
+  ApiRequestADI.fromJson(Map<String, dynamic> json)
+      : url = json['url'],
+        publicKeyHash = json['publicKeyHash'];
+
+  Map<String, dynamic> toJson() => {'url': url, 'publicKeyHash': publicKeyHash};
+}
