0deff57024cd327f302690b38b60e9db6d36e71d
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2022-01-10 16:30:23 +0200

    add json rpc handler

--- lib/accumulate.dart
+++ lib/accumulate.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library accumulate;
 
-export 'src/accumulate_base.dart';
+export 'src/api.dart';
 
 // TODO: Export any libraries intended for clients of this package.
