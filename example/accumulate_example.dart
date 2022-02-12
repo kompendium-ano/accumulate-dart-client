32a3f7eddf9a3d1508fb330cf92db9054759e1e5
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2022-02-12 17:48:41 +0200

    add credits example

--- example/accumulate_example.dart
+++ example/accumulate_example.dart
@@ -13,7 +13,7 @@ import 'package:tuple/tuple.dart';
 
 void main() {
   group('DevNet Tests', () {
-    final acmeAPI = ACMEApiV2("https://testnet.accumulatenetwork.io/", "v2");
+    final testnetAPI = ACMEApiV2("https://testnet.accumulatenetwork.io/", "v2");
 
     setUp(() async {});
 
@@ -42,29 +42,52 @@ void main() {
     }
 
     Future<String> makeCreditsTest() async {
-      // Generate some random data for private keys
+      // 1. Generate some random data for private keys
       String mnemonic = bip39.generateMnemonic();
       Uint8List seed = bip39.mnemonicToSeed(mnemonic);
 
-      // Additional setup goes here.
-      // 1. initiate public/private keypage
+      // 2. initiate public/private keypage
       var privateKey = ed.newKeyFromSeed(seed.sublist(0, 32)); // Uint8List.fromList(List.generate(32, (index) => 0)));
       var publicKey = ed.public(privateKey);
 
-      // 2. Create New unique ACME url based on Protocol definition
+      // 3. Create New unique ACME url based on Protocol definition
       Address liteAccount = Address("", "ACME Account", "");
       AccumulateURL currentURL = liteAccount.generateAddressViaProtocol(publicKey.bytes, "ACME");
       liteAccount.address = currentURL.getPath();
       liteAccount.URL = currentURL;
 
+      //   3.1 Store associated private key data along with Address structure
+      //       internal methods will use it to sign payloads
+      liteAccount.puk = HEX.encode(publicKey.bytes);
+      liteAccount.pik = privateKey.bytes;
+      liteAccount.pikHex = HEX.encode(privateKey.bytes);
+
       print(liteAccount.address);
 
-      // 3. Initiate API class instance and register address on the network with faucet
-      final acmeAPI = ACMEApiV2("https://devnet.accumulatenetwork.io/", "v2");
-      final resp = await acmeAPI.callFaucet(liteAccount);
-      return resp;
+      // 4. Initiate API class instance and register address on the network with faucet
+      final acmeAPI = ACMEApiV2("http://178.20.158.25:56660/", "v2");
+      final respFaucet = await acmeAPI.callFaucet(liteAccount);
+      print('faucet - ${respFaucet}');
+
+      // 5. Wait at least 4 seconds for a transaction to settle on the network
+      final sleep = await Future.delayed(Duration(seconds: 5));
+      final resptx = await acmeAPI.callGetTokenTransaction(respFaucet);
+
+      // 6. Credits are converted from ACME token
+      //   6.1 Get current timestamp in microseconds it works as Nonce
+      int timestamp = DateTime.now().toUtc().millisecondsSinceEpoch;
+
+      //   6.2 Execute actual credits call
+      final respCredits = await acmeAPI.callAddCredits(liteAccount, 1000, timestamp);
+      print('credits - ${respCredits}');
+
+      //   6.3 Check that balance indeed updated
+      final respAccount = await acmeAPI.callQuery(liteAccount.address);
+      print(respAccount.creditBalance.toString());
+
+      return respAccount.url;
     }
-    
+
     Future<String> makeAdiTest() async {
       Address currAddr = Address("acc://dfdc4d9ec31f8dce909cdcaba2ec1d17ccd07f49fdb2bff0/acme", "web1", "");
       currAddr
@@ -82,12 +105,12 @@ void main() {
 
       // 3. add timestamp
       int timestamp = DateTime.now().toUtc().millisecondsSinceEpoch;
-      int timestampShort = (DateTime.now().millisecondsSinceEpoch / 1000).toInt();
+      //int timestampShort = (DateTime.now().millisecondsSinceEpoch / 1000).toInt();
 
       final acmeAPI = ACMEApiV2("https://devnet.accumulatenetwork.io/", "v2");
       String txhash = "";
       try {
-        final resp = await acmeAPI.callCreateAdi(currAddr, newADI, timestampShort);
+        final resp = await acmeAPI.callCreateAdi(currAddr, newADI, timestamp);
         txhash = resp;
       } catch (e) {
         e.toString();
@@ -95,23 +118,24 @@ void main() {
       }
 
       return txhash;
-     }
-
+    }
 
     test('Faucet Test - Single Call', () async {
       final String resp = await makeFaucetTest();
       expect(resp.isNotEmpty, isTrue);
+      final String respC = await makeCreditsTest();
+      expect(respC.isNotEmpty, isTrue);
     });
 
-    test('Faucet Test - Multiple Calls Over Short Period', () async {
-      final String resp = await makeFaucetTest();
-      for (int i = 0; i < 10; i++) {
-        final String resp = await makeFaucetTest();
-        final sleep = await Future.delayed(Duration(seconds: 1));
-      }
-      expect(resp.isNotEmpty, isTrue);
-    });
+    // test('Faucet Test - Multiple Calls Over Short Period', () async {
+    //   // final String resp = await makeFaucetTest();
+    //   // for (int i = 0; i < 10; i++) {
+    //   //   final String resp = await makeFaucetTest();
+    //   //   final sleep = await Future.delayed(Duration(seconds: 1));
+    //   // }
+    //   // expect(resp.isNotEmpty, isTrue);
+    // });
 
-    exit(0);
+    //exit(0);
   });
 }
