cf3a74d8caac0f7f31c8114aaf8b2a6b2fb4c9f2
Jimmy <jimmy@snapworks.me>
2022-04-13 00:48:48 +0530

    ApiRequestWriteData Fix

--- lib/src/v2/requests/api_request_write_data.dart
+++ lib/src/v2/requests/api_request_write_data.dart
@@ -2,30 +2,13 @@ class ApiRequestWriteData {
   final String? data;
   final List<String>? extIds;
 
-  /*
-  ApiRequestWriteData(this.data,this.extIds);
-
-  ApiRequestWriteData.fromJson(Map<String, dynamic> json) : data = json['entry']['data'],
-        extIds = json['extIDs'];
-
-  Map<String, dynamic> toJson() => {
-        'entry': {'data': data}
-      };*/
-
   ApiRequestWriteData(this.data, this.extIds);
 
   ApiRequestWriteData.fromJson(Map<String, dynamic> json)
       : data = json['entry']['data'],
-        extIds = json['extIDs'];
+        extIds = json['entry']['extIds'];
 
-  /*
   Map<String, dynamic> toJson() => {
-        'entry': {'data': data},
-        'extIds': extIds
-      };
-*/
-  Map<String, dynamic> toJson() => {
-        'entry': {'data': data},
-        'extIds': {'extIds': extIds}
+        'entry': {'data': data, 'extIds': extIds},
       };
 }
