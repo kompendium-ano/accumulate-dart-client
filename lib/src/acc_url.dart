bb95ad83cb8c945346067304e7f63004e7e39bfc
Jimmy Jose <theguywhomakesapp@gmail.com>
2022-06-24 20:14:08 +0530

    Fix acc_url.g.dart

--- lib/src/acc_url.dart
+++ lib/src/acc_url.dart
@@ -1,12 +1,16 @@
 import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
-//part 'acc_url.g.dart';
+part 'acc_url.g.dart';
 
 @HiveType(typeId: 666)
 class AccURL  extends HiveObject with EquatableMixin {
+  @HiveField(0)
   late String authority;
+  @HiveField(1)
   late String path;
+  @HiveField(2)
   late String query;
+  @HiveField(3)
   late String fragment;
 
   AccURL(String url) {
