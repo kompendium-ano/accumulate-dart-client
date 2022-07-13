284069154fc341ebecfb471bb65e3b76d384e9f1
Jimmy Jose <theguywhomakesapp@gmail.com>
2022-07-13 18:49:48 +0530

    Reformat writedata

--- lib/src/payload/write_data.dart
+++ lib/src/payload/write_data.dart
@@ -43,50 +43,46 @@ class WriteData extends BasePayload {
     return _dataHash!;
   }
 
-
-  Uint8List marshalDataEntry(List<Uint8List> data){
+  Uint8List marshalDataEntry(List<Uint8List> data) {
     List<int> forConcat = [];
 
-  // AccumulateDataEntry DataEntryType 2
-  forConcat.addAll(uvarintMarshalBinary(2, 1));
-  // Data
-    for(Uint8List val in data){
+    // AccumulateDataEntry DataEntryType 2
+    forConcat.addAll(uvarintMarshalBinary(2, 1));
+    // Data
+    for (Uint8List val in data) {
       forConcat.addAll(bytesMarshalBinary(val, 3));
     }
 
+    return bytesMarshalBinary(forConcat.asUint8List());
+  }
 
-  return bytesMarshalBinary(forConcat.asUint8List());
-}
-
-  Uint8List sha256Update(Uint8List data){
+  Uint8List sha256Update(Uint8List data) {
     return sha256.convert(data).bytes.asUint8List();
   }
 
-  Uint8List hashTree(List<Uint8List> items){
-  final hashes = items.map((i) => sha256Update(i)).toList();
+  Uint8List hashTree(List<Uint8List> items) {
+    final hashes = items.map((i) => sha256Update(i)).toList();
 
-  while (hashes.length > 1) {
-  var i = 0; // hashes index
-  var p = 0; // pointer
-  while (i < hashes.length) {
-  if (i == hashes.length - 1) {
-  // Move the last "alone" leaf to the pointer
-  hashes[p] = hashes[i];
-  i += 1;
-  } else {
-    List<int> forConcat = [];
-    forConcat.addAll(hashes[i]);
-    forConcat.addAll(hashes[i+1]);
-  hashes[p] = sha256Update(forConcat.asUint8List());
-  i += 2;
+    while (hashes.length > 1) {
+      var i = 0; // hashes index
+      var p = 0; // pointer
+      while (i < hashes.length) {
+        if (i == hashes.length - 1) {
+          // Move the last "alone" leaf to the pointer
+          hashes[p] = hashes[i];
+          i += 1;
+        } else {
+          List<int> forConcat = [];
+          forConcat.addAll(hashes[i]);
+          forConcat.addAll(hashes[i + 1]);
+          hashes[p] = sha256Update(forConcat.asUint8List());
+          i += 2;
+        }
+        p += 1;
+      }
+      hashes.length = p;
+    }
+
+    return hashes[0];
   }
-  p += 1;
-  }
-  hashes.length = p;
-  }
-
-  return hashes[0];
-}
-
-
 }
