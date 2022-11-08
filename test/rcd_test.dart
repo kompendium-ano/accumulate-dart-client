4d217bb079d20a6f0d0c41025078e673d1dea21e
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2022-11-08 20:01:01 +0200

    add some tests

--- test/rcd_test.dart
+++ test/rcd_test.dart
@@ -2,18 +2,22 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:accumulate_api6/accumulate_api6.dart';
-import 'package:accumulate_api6/src/model/address.dart';
+import 'package:accumulate_api6/src/model/factom/factom_scanner.dart';
 import 'package:accumulate_api6/src/signing/rcd.dart';
-import 'package:accumulate_api6/src/utils/merkle_root_builder.dart';
 import 'package:accumulate_api6/src/utils/utils.dart';
 import 'package:bip39/bip39.dart' as bip39;
 import 'package:bitcoin_bip44/bitcoin_bip44.dart';
 import 'package:test/test.dart';
 
-
 // seed - carry bullet century olympic core drift between axis draw pilot pluck wash
 //
 void main() {
+
+  handleView(Address value){
+    //print(value.publicKey.getEncoded());
+    print(getFactoidAddressFromRCDHash(getRCDHashFromPublicKey(value.publicKey.getEncoded(false), 1)));
+  }
+
   test('should construct RCD hash', () {
 
     String fa = "FA27j6v5HGrG987D5ksQvgHc5jpPww7dhB1VuYRSrxm9rEmANatK";
@@ -29,10 +33,14 @@ void main() {
       print("LITE ACC: $la");
     }
 
-    var factoidInfo = getFactoidAddressRcdHashPkeyFromPrivateFs("Fs2yxj5FYs2VhYrpHT6VQ3n8fjaGJCCFA79n4ZKN8PiiczxHeXH7");
-    print(factoidInfo.faAddress);
+    String fs = "Fs2yxj5FYs2VhYrpHT6VQ3n8fjaGJCCFA79n4ZKN8PiiczxHeXH7";
+    var factoidInfo = getFactoidAddressRcdHashPkeyFromPrivateFs(fs);
+    print("FA FROM  FS: ${factoidInfo.faAddress}");
     print("RCD from FA: $rcdHash");
     print("RCD from FS: ${factoidInfo.rcdHash}");
+    print("FA FROM RCD: ${getFactoidAddressFromRCDHash(factoidInfo.rcdHash!)}");
+
+
     print("----------------------------------------------");
     print("Reconstruct ED25519");
 
@@ -52,46 +60,59 @@ void main() {
     print("LID url from Signer: ${lid.url}");
     print("LID url from FA:     ${lidFromFA}");
 
-
-
     ///////////////////////////////////////////////////////////////
 
-
     var secret = getFactoidSecretFromPrivKey(factoidInfo.keypair!.secretKey);
-    print(secret);
+    print("SECRET ORIGINAL  : $fs");
+    print("SECRET FROM PRIVK: ${secret}");
 
-    print("----------------------------------------------");
+    print("\n----------------------------------------------");
     print("| Mnemonics \n");
     // Mnemonic workflow
-    String mnemonic = "";
+
+    String mnemonic = "carry bullet century olympic core drift between axis draw pilot pluck wash";
     List<String> seedPhraseWords =
         ["carry", "bullet", "century", "olympic", "core", "drift", "between", "axis", "draw", "pilot", "pluck", "wash"];
 
+    // Add a scanner of your own:
+    scanners = [FactomScanner()];
+    var bip44 = Bip44(bip39.mnemonicToSeedHex(mnemonic));
+    var bip44W = Bip44(toHexString(mnemonic));
+    bip44.hashCode;
+    bip44W.hashCode;
 
-    //var seed = bip44.mnemonicToSeed("carry bullet century olympic core drift between axis draw pilot pluck wash");
-    var bip44 = Bip44(toHexString('carry bullet century olympic core drift between axis draw pilot pluck wash'));
-    var factoid = bip44.coins[131];
-    var account = Account(factoid, 0, changeExternal);
+    var factoid = bip44.coins[130];
+    print(factoid.path);
 
-    account.usedAddresses(0).then(print);
+    for (var i = 0; i < 100; i++) {
+      var account = Account(factoid, i, 0);
+      account.nextUnusedAddress(0).then((value) => handleView(value));
 
-    //Ed25519Keypair ed25519keypair = Ed25519Keypair.fromSeed(seed);
-    //var secret2 = getFactoidSecretFromPrivKey(ed25519keypair.secretKey);
+      // ExtendedKey? key44 = account.chain.forPath("m/44'/131'/0'/0/0");
+      // const String alphabet =
+      //     '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
+      // List<int> s = Base58Codec(alphabet).decode(key44.toString());
+      // Ed25519Keypair kp = Ed25519Keypair.fromSecretKey(s.asUint8List());
+
+    }
+
+
+    var seed = bip39.mnemonicToSeed("carry bullet century olympic core drift between axis draw pilot pluck wash");
+    Ed25519Keypair ed25519keypair2 = Ed25519Keypair.fromSeed(seed);
+    var secret2 = getFactoidSecretFromPrivKey(ed25519keypair2.secretKey);
+
+    print("CORRECT: Fs2yxj5FYs2VhYrpHT6VQ3n8fjaGJCCFA79n4ZKN8PiiczxHeXH7");
+    print("CALC:    $secret2");
 
     //var rcd2 = getRCDHashFromPublicKey(ed25519keypair.publicKey, 1);
     //var addr = getFactoidAddressFromRCDHash(rcd2);
     //print("$secret2");
+    print("CALC: ${factoidInfo.faAddress}");
 
     expect(fa, factoidInfo.faAddress);
 
   });
 
 
-}
 
-String toHexString(String original) {
-  return original.codeUnits
-      .map((c) => c.toRadixString(16).padLeft(2, '0'))
-      .toList()
-      .join('');
 }
