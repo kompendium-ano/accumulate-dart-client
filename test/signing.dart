2cd42e391b58ed26bd4d28f3d99013cf4e46db1d
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2023-05-13 19:44:05 +0300

    update tests

---
+++ test/signing.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:accumulate_api/accumulate_api.dart';
+import 'package:accumulate_api/src/utils/merkle_root_builder.dart';
+import 'package:accumulate_api/src/utils/proof.dart';
+import 'package:accumulate_api/src/utils/utils.dart';
+import 'package:test/test.dart';
+import 'package:hex/hex.dart';
+void main() {
+
+  test('should sign tx', () {
+
+
+    // assert entries
+
+
+  });
+
+  test('should sign tx with delegate', () {
+
+
+    // assert entries
+
+
+  });
+
+}
